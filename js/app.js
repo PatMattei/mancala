7bd1b73afbc50fec72bd083c8176b9c41d9066f5
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-01 17:29:33 -0400

    moveStones function working

--- js/app.js
+++ js/app.js
@@ -1,35 +1,55 @@
 const board = {
-    a1: 4,
-    a2: 4,
-    a3: 4,
-    a4: 4,
-    a5: 4,
-    a6: 4,
-    b1: 4,
-    b2: 4,
-    b3: 4,
-    b4: 4,
-    b5: 4,
-    b6: 4,
+    1: 4,
+    2: 4,
+    3: 4,
+    4: 4,
+    5: 4,
+    6: 4,
+    7: 4,
+    8: 4,
+    9: 4,
+    10: 4,
+    11: 4,
+    12: 4,
     storeA: 0,
     storeB: 0
 }
 
-const countStones = (space) => {
-    const $space = $(space);
-    const spaceName = $space.attr('id');
+const getStoneCount = (pitId) => {
+    return board[pitId];
+};
 
-    $space.html(board[spaceName]);
+
+const updateBoard = (space) => {
+    const $space = $(space);
+    const spaceName = $space.attr('pit-position');
+
+    $space.html(`${getStoneCount(spaceName)}<br />${spaceName}`);
 };
 
 const moveStones = (event) => {
     $targetPit = $(event.currentTarget);
-    const stones = $targetPit.attr('contents');
+    pitPosition = $targetPit.attr('pit-position');
 
-    board[$targetPit.attr('id')] = 0;
-    countStones($targetPit)
+    const spaceName = $targetPit.attr('pit-position');
+    const stones = getStoneCount(spaceName);
 
-    
+    board[$targetPit.attr('pit-position')] = 0; //remvoe the stones from pit
+    updateBoard($targetPit);
+
+    //move stone to neihgboring pits
+    for (let i = 0; i < stones; i++) {
+        //new target is clockwise pit
+        pitPosition--
+        if (pitPosition < 1) {
+            pitPosition = 12;
+        }
+
+        const $newTarget = $(`.pit[pit-position="${pitPosition}"]`);
+        board[pitPosition]++
+        updateBoard($newTarget);
+
+    }
 };
 
 
@@ -38,10 +58,10 @@ const moveStones = (event) => {
 $(() => {
     //iterate through each pit
     $('.pit').each((i, pit) => {
-        countStones(pit);
+        updateBoard(pit);
     });
     $('.mancala-store').each((i, store) => {
-        countStones(store);
+        updateBoard(store);
     });
 
     //on clicking a pit
