dd52e56a46b03a8382b52b6e336b8697f7aa906e
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-03 11:12:35 -0400

    moving stones working with objects

--- js/app.js
+++ js/app.js
@@ -1,28 +1,14 @@
-const board = {
-    1: 4,
-    2: 4,
-    3: 4,
-    4: 4,
-    5: 4,
-    6: 4,
-    7: 4,
-    8: 4,
-    9: 4,
-    10: 4,
-    11: 4,
-    12: 4,
-    storeA: 0,
-    storeB: 0
-}
-
 class Space {
 	constructor(stones, type, name) {
-        this.stones = stones;
+        this.stoneCount = stones;
         this.type = type;
         this.name = name;
     }
     addStone() {
-        this.stones++;
+        this.stoneCount++;
+    }
+    removeStones() {
+        this.stoneCount = 0;
     }
 }
 
@@ -30,6 +16,16 @@ class Store extends Space {
     constructor(stones, type, name) {
         super(stones, type, name);
     }
+}
+
+class Factory {
+	constructor() {
+	}
+	generatePit(i) {
+		const newPit = new Space(4, "pit", `${i}`);
+		enemyList.push(newPit);
+	}
+
 }
 
 const a1 = new Space(4, "pit", "a1");
@@ -46,48 +42,48 @@ const b5 = new Space(4, "pit", "b5");
 const b6 = new Space(4, "pit", "b6");
 const storeA = new Store(0, "store", "storeA");
 const storeB = new Store(0, "store", "storeB");
-const boardOrder = [storeA, a1, a2, a3, a4, a5, a6, storeB, b1, b2, b3, b4, b5, b6];
+const board = {"a1": a1, a1,"a2": a2,"a3": a3,"a4": a4,"a5": a5,"a6": a6,"b1": b1,"b2": b2,"b3": b3,"b4": b4,"b5": b5,"b6": b6,"storeA": storeA,"storeB": storeB};
+
+const boardOrder = ["storeA", "a1", "a2", "a3", "a4", "a5", "a6", "storeB", "b1", "b2", "b3", "b4", "b5", "b6"];
 
 
-const getStoneCount = (pitId) => {
-    return board[pitId];
-};
-
-
-const updateBoard = (space) => {
+const  updateBoard = (space) => {
     const $space = $(`#${space.name}`);
-    const stones = space.stones;
 
-    $space.html(`${space.stones}<br />${space.name}`);
+    $space.html(`${space.stoneCount}<br />${space.name}`);
 };
 
 const moveStones = (event) => {
     $targetPit = $(event.currentTarget);
-    pitPosition = $targetPit.attr('pit-position');
+    let id = $targetPit.attr('id');
+    let stonesInHand = board[id].stoneCount;
 
-    const spaceName = $targetPit.attr('pit-position');
-    let stones = getStoneCount(spaceName);
+    board[id].removeStones();
+    updateBoard(board[id]);
 
-    board[$targetPit.attr('pit-position')] = 0; //remvoe the stones from pit
-    updateBoard($targetPit);
 
     //move stone to neihgboring pits
-   while (stones > 0) {
+    while (stonesInHand > 0) {
         //new target is clockwise pit
-        pitPosition--
-        if (pitPosition < 1 && stones == 1) {
-            board.storeA++;
-            updateBoard($('#storeA'));
-        }
-        else if (pitPosition < 1) {
-            pitPosition = 12;
-        }
-
-        const $newTarget = $(`.pit[pit-position="${pitPosition}"]`);
-        board[pitPosition]++
-        updateBoard($newTarget);
-        stones--;
+        const target = boardOrder.indexOf(id) - 1;
+        id = boardOrder[target];
+        board[id].addStone();
+        stonesInHand--;
+        updateBoard(board[id]);
     }
+//         if (pitPosition < 1 && stones == 1) {
+//             board.storeA++;
+//             updateBoard($('#storeA'));
+//         }
+//         else if (pitPosition < 1) {
+//             pitPosition = 12;
+//         }
+
+//         const $newTarget = $(`.pit[pit-position="${pitPosition}"]`);
+//         board[pitPosition]++
+//         updateBoard($newTarget);
+//         stones--;
+//     }
 };
 
 
@@ -95,7 +91,7 @@ const moveStones = (event) => {
 
 $(() => {
     //iterate through each space
-    boardOrder.forEach(space => updateBoard(space))
+    boardOrder.forEach(space => updateBoard(board[space]))
 
     //on clicking a pit
     $('.pit').on('click', moveStones);
